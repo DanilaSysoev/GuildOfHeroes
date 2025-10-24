dcdb0e521fc45ec97789969ac96cf6e895dd009d
Данила Сысоев <crocodil-msu@ya.ru>
2025-10-24 22:25:48 +0300

    Transform ensure_parent_dirs

--- ui/src/utils.rs
+++ ui/src/utils.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::io;
 use std::path::Path;
 
-pub fn ensure_parent_dirs<P: AsRef<Path>>(file_path: P) -> io::Result<()> {
+pub fn ensure_parent_dirs(file_path: impl AsRef<Path>) -> io::Result<()> {
     if let Some(parent) = file_path.as_ref().parent()
         && !parent.exists()
     {
